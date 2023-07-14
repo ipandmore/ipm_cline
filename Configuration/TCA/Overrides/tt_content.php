4a5b44e30e895a382bac30c7dcec7eed8d1ffd6b
Developer <developer@ipandmore.de>
2023-07-14 10:43:17 +0000

    [RELEASE] New version 4.0.2

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -1,8 +1,12 @@
 <?php
 defined('TYPO3') || die();
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+$pluginSignature = \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
     'IpmCline',
     'Cline',
     'C-Line'
 );
+
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'layout,select_key,pages';
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature, 'FILE:EXT:ipm_cline/Configuration/FlexForm/Cline.xml');
--- ext_tables.php
+++ ext_tables.php
@@ -1,16 +1,3 @@
 <?php
 defined('TYPO3') || die();
 
-call_user_func(
-    function ($extKey) {
-
-        $pluginName = 'Cline';
-
-        $pluginSignature = strtolower(\TYPO3\CMS\Core\Utility\GeneralUtility::underscoredToUpperCamelCase($extKey)) .'_' . strtolower($pluginName);
-
-        $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'layout,select_key,pages';
-        $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature, 'FILE:EXT:' . $extKey . '/Configuration/FlexForm/Cline.xml');
-    }, 'ipm_cline'
-);
-
